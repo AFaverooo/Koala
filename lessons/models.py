94bd7d46c35ea4c2889987307233c4846478eb90
AUGUSTO FAVERO <augusto.favero@kcl.ac.uk>
2022-11-13 19:04:38 +0000

    Create Student model

--- lessons/models.py
+++ lessons/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator,MaxValueValidator,MinValueValidator
 
-# Create your models here.
+#Student model refers to the User of the MSMS application
+class Student(AbstractUser):
+    student_ethinicity = models.CharField(max_length = 30, blank = False)
+    student_gender = models.CharField(max_length = 20, blank = False)
+    student_age = models.PositiveIntegerField(blank=False, validators = [MinValueValidator(18), MaxValueValidator(70)])
