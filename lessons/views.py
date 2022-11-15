4d3c63b38675c809bbc8d08ea20b818601227e73
Saathveekan Satheeshkumar <saathveekan.satheeshkumar@kcl.ac.uk>
2022-11-15 21:30:41 +0000

    Create sign up form
    - Create forms.py
    -Add test_sign_up_form

--- lessons/views.py
+++ lessons/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .forms import SignUpForm
 # Create your views here.
 
 
@@ -7,3 +7,15 @@ from django.shortcuts import render
 
 def home(request):
 	return render(request, 'home.html')
+
+def sign_up(request):
+
+	if(request.method == 'GET'):
+		form = SignUpForm()
+	if(request.method == 'POST'):
+		form = SignUpForm(request.POST)
+		if form.is_valid():
+			student = form.save()
+			login(request, student)
+
+	return render(request, 'sign_up.html', {'form': form})
