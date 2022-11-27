d20225b4f45e0428dc11487dcf72a1228a4208f0
BOWEN ZHU <bowen.zhu@kcl.ac.uk>
2022-11-27 23:39:22 +0000

    implemented tests for invoice

--- lessons/tests/views/test_invoice_view.py
+++ lessons/tests/views/test_invoice_view.py
@@ -1,16 +1,39 @@
 from django.test import TestCase
-from lessons.models import Invoice
+from lessons.models import Invoice, InvoiceStatus, UserAccount, Gender
+from django.urls import reverse
 
-class invoiceTestCase(TestCase): 
+class InvoiceTestCase(TestCase): 
+    '''Tests of the invoice view'''
 
-    def test_reference_must_not_be_blank(self):
-        pass
+    def setUp(self):
+        self.url = reverse('invoice')
 
-    def test_reference_must_be_in_the_correct_form(test):
-        pass
+        self.student = UserAccount.objects.create_student(
+            first_name='John',
+            last_name='Doe',
+            email='johndoe@example.org',
+            password='Password123',
+            gender = Gender.MALE,
+        )
 
-    def fees_cannot_be_0():
-        pass
+        self.invoice = Invoice.objects.create(
+            reference_number = '111-001',
+            student_ID = '111',
+            fees_amount = '78',
+            invoice_status = InvoiceStatus.UNPAID,
+        )
+        self.invoice = Invoice.objects.get(reference_number = '111-001')
 
-    def there_can_only_be_one_slush():
-        pass
+    def test_invoice_url(self):
+        self.assertEqual(self.url, '/invoice/')
+
+    def test_get_invoice(self):
+        self.client.login(username=self.student.email, password='Password123')
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'invoice.html')
+
+
+
+
+
