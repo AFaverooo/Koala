bd491b807a56bf4ee527c9850929b36589f36ea0
KARIM MOUSA <karim.mousa@kcl.ac.uk>
2022-12-01 14:36:57 +0000

    Add all tests templates to implement

--- lessons/tests/views/test_director_role_management_page.py
+++ lessons/tests/views/test_director_role_management_page.py
@@ -1,12 +1,22 @@
 from django.test import TestCase
 from lessons.tests.helpers import LogInTester, reverse_with_next
 from django.urls import reverse
+from lessons.views import promote_admin, promote_director
 
 class DirectorRoleChangesTestCase(TestCase):
     """Tests for the director_manage_roles view."""
 
     def setUp(self):
         self.url = reverse('director_manage_roles')
+
+        self.current_user = UserAccount.objects.create_admin(
+            first_name='Ahmed',
+            last_name='Ali',
+            email='ahmedali@example.org',
+            password='Password123',
+            gender = 'M',
+        )
+
         self.admin = UserAccount.objects.create_admin(
             first_name='Jane',
             last_name='Doe',
@@ -27,5 +37,52 @@ class DirectorRoleChangesTestCase(TestCase):
         self.assertEqual(self.url,'/director_manage_roles/')
 
 
+    # Test for behaviors of the widgets related to each user
+
     def test_promote_admin_to_director(self):
-        self.assertEqual(self.url,'/director_manage_roles/')
+        pass
+
+    def test_demote_director_to_admin(self):
+        pass
+
+
+    def test_edit_director_info(self):
+        pass
+
+    def test_edit_admin_info(self):
+        pass
+
+
+    def test_disable_director_info(self):
+        pass
+
+    def test_disable_admin_info(self):
+        pass
+
+
+    def test_delete_director(self):
+        pass
+
+    def test_delete_admin(self):
+        pass
+
+
+    # Prevent/ allow actions dont on the current user
+
+    def current_user_can_edit_himself(self):
+        pass
+
+    def current_user_cant_promote_himself(self):
+        pass
+
+    def current_user_demote_promote_himself(self):
+        pass
+
+    def current_user_cant_edit_himself(self):
+        pass
+
+    def current_user_cant_delete_himself(self):
+        pass
+
+    def current_user_cant_disable_himself(self):
+        pass
