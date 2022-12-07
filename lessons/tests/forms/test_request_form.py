76fc75837ed4e49d835e2704d4a4ec821c1a466c
Saathveekan Satheeshkumar <saathveekan.satheeshkumar@kcl.ac.uk>
2022-12-07 15:13:42 +0000

    Add fixtures for test_login_form and test_request_form

--- lessons/tests/forms/test_request_form.py
+++ lessons/tests/forms/test_request_form.py
@@ -4,29 +4,36 @@ from django.test import TestCase
 from lessons.models import Lesson, LessonType,LessonDuration,LessonStatus,Gender, UserAccount
 import datetime
 from django.utils import timezone
-
+from django.urls import reverse
 
 
 class RequestFormTestCase(TestCase):
     """Unit tests of the sign up form."""
 
+    fixtures = ['lessons/tests/fixtures/useraccounts.json']
+
+
+    
     def setUp(self):
 
-        self.teacher = UserAccount.objects.create_teacher(
-            first_name='Barbare',
-            last_name='Dutch',
-            email='barbdutch@example.org',
-            password='Password123',
-            gender = Gender.FEMALE,
-        )
+        self.teacher = UserAccount.objects.get(email='barbdutch@example.org')
+        self.student = UserAccount.objects.get(email='johndoe@example.org')
+        # self.teacher = UserAccount.objects.create_teacher(
+        #     first_name='Barbare',
+        #     last_name='Dutch',
+        #     email='barbdutch@example.org',
+        #     password='Password123',
+        #     gender = Gender.FEMALE,
+        # )
         self.form_input = {
-            'type':LessonType.INSTRUMENT,
-            'duration':LessonDuration.THIRTY,
-            'lesson_date_time':datetime.datetime(2022, 11, 20, 15, 15, 00, tzinfo=timezone.utc),
-            'teachers': self.teacher
+            'type': LessonType.INSTRUMENT,
+            'duration': LessonDuration.THIRTY,
+            'lesson_date_time' : datetime.datetime(2023, 4, 4, 15, 15, 15, tzinfo=timezone.utc),
+            'teachers': self.teacher.id,
+            'selectedStudent': self.student.email,
         }
 
-
+        self.url = reverse('new_lesson')
     def test_valid_request_form(self):
         form = RequestForm(data = self.form_input)
         self.assertTrue(form.is_valid())
@@ -41,20 +48,23 @@ class RequestFormTestCase(TestCase):
         self.assertTrue(isinstance(date_time_field, forms.DateTimeField))
 
 
-    ##form.save() causes an error
-
-    # def test_form_must_save_correctly(self):
-    #     form = RequestForm(data=self.form_input)
+    # def test_succesful_request(self):
+    #     self.client.login(email=self.student.email, password="Password123")
     #     before_count = Lesson.objects.count()
-    #     ##This causes an error
-    #     form.save()
+    #     response = self.client.post(self.url, self.form_input, follow = True)
     #     after_count = Lesson.objects.count()
     #     self.assertEqual(after_count, before_count+1)
-    #     lesson = Lesson.objects.get(lesson_date_time =datetime.datetime(2022, 11, 20, 15, 15, 00, tzinfo=timezone.utc))
-    #     self.assertEqual(lesson.type, LessonType.INSTRUMENT)
-    #     self.assertEqual(lesson.duration, LessonDuration.THIRTY)
-    #     self.assertEqual(lesson.teachers.email, 'barbdutch@example.org')
-    #     self.assertEqual(lesson.teachers.first_name, 'Barbare')
-    #     self.assertEqual(lesson.teachers.last_name, 'Dutch')
-    #     self.assertEqual(lesson.teachers.gender, Gender.FEMALE)
-      
+    #     form = response.context['form']
+    #     self.assertTrue(isinstance(form, RequestForm))
+    #     self.assertFalse(form.is_bound)
+
+    #     lessons = response.context['lessons']
+    #     self.assertEqual(len(lessons),1)
+    #     #check lessons size is now 4 for the user
+    #     self.assertEqual(lessons[0].student_id.email, self.student.email)
+    #     self.assertEqual(lessons[0].type,LessonType.INSTRUMENT)
+    #     self.assertEqual(lessons[0].duration, LessonDuration.THIRTY)
+    #     self.assertEqual(lessons[0].lesson_date_time, datetime.datetime(2023, 4, 4, 15, 15, 15, tzinfo=timezone.utc))
+    #     self.assertEqual(lessons[0].teacher_id, self.teacher)
+
+    #     self.assertTemplateUsed(response, 'requests_page.html')
